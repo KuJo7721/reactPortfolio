c50f9efed8e7dfbc9372b0ed7fa5049b71ebfc64
KJ Krueger <kjkrueger12@gmail.com>
2022-10-13 21:13:50 -0700

    updated contacts

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,20 +3,15 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+      <h1>Contact</h1>
+        <p>Phone:(939)344-0549</p>
+        <p>Email: kjkrueger12@gmail.com</p>
+        <ul class="list-group list-group-flush">
+                    <li class="list-group-item text-center"><a href="https://github.com/KuJo7721">Git Hub</a></li>
+                    <li class="list-group-item text-center"><a href="https://www.linkedin.com/in/kj-krueger-3b50a4225/">LinkedIn</a></li>
+                    <li class="list-group-item text-center"><a href="https://docs.google.com/document/d/1I6s1M8a4S5MjaDM3ocnZZ6gAL-p-AMiWt5ohuUCcAoE/edit?usp=sharing">Resume</a></li>
+                   
+                </ul>
     </div>
   );
 }
